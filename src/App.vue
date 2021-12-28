99c8c3ee986424d6e8af8e611410952aad7b1094
Pedro Lucas <pedro.santos@me.com.br>
2021-12-28 15:03:59 -0300

    Clear Project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <h1>Cheguei aqui</h1>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
